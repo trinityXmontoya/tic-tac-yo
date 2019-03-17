7a998a49c154d5903951b43a3fe4e049cd7e2f86
trinity <trinityxmontoya@gmail.com>
2019-03-17 19:35:02 -0400

    add aot to profiles

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:uberjar {:aot [tic-tac-yo.core]}}
   :main tic-tac-yo.core)

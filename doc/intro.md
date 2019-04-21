d101b1580a5b0c2f0c4b6f7d60de3c8b84c645b4
trinity <trinityxmontoya@gmail.com>
2019-04-20 20:50:50 -0400

    cleanup

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to tic-tac-yo
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/tic_tac_yo/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns tic-tac-yo.core-test
-  (:require [clojure.test :refer :all]
-            [tic-tac-yo.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
